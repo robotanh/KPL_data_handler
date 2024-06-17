dfbf474335f8c85a115e01725f6cf006730c3980
robotanh <robotanh15@gmail.com>
2024-06-17 13:11:00 +0000

    config firebase cpp

---
+++ build/CMakeFiles/KPL_data_handler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KPL_data_handler.dir/src/main.cpp.o"
+  "CMakeFiles/KPL_data_handler.dir/src/main.cpp.o.d"
+  "KPL_data_handler"
+  "KPL_data_handler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KPL_data_handler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/KPL_data_handler.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for KPL_data_handler.
+# This may be replaced when dependencies are built.
